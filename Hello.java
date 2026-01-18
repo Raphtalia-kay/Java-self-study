94174490dbe4661066cf50cecde3e8b4a617a3d1
Your Name <your-email@example.com>
2026-01-19 00:41:56 +0700

    Day1 : Java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+        // System.out.print("Hello World");
+        // System.out.println(4 + 3);
+        // System.out.println(5 + 3);
+
+    }
+}

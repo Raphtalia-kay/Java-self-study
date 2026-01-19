f635595451c526fef5dd4b039e2e77670d177088
Your Name <your-email@example.com>
2026-01-19 23:09:32 +0700

    Day2 : Java

--- Hello.java
+++ Hello.java
@@ -5,7 +5,25 @@ class Hello
         int num1 = 3;
         int num2 = 5;
         int result = num1 + num2;
+        byte b1 = 125;
+        short s1= 32767;
+        long l1 = 10L;
+
+        float f1 = 3.14f;
+        double d1 = 3.14;
+
+        char c1 = 'k';
+
+        boolean b2 = true;
+
         System.out.println(result);
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(c1);
+        System.out.println(b2);
         // System.out.print("Hello World");
         // System.out.println(4 + 3);
         // System.out.println(5 + 3);
---
+++ Literal.java
@@ -0,0 +1,41 @@
+public class Literal {
+    public static void main(String a[]) {
+        // literal
+        // int number = 10_10_10_100;
+        // int number = 0b101;
+        int number = 0x7E;
+        System.out.println(number);
+
+        // double number1 = 55;
+        double number1 = 10e10;
+        System.out.println(number1);
+
+        char c = 'a';
+        c++;
+        System.out.println(c);
+        
+        // Type Conversion - change data types to another automatically
+        // Type Casting - force to change data types to another
+
+        // Type Conversion
+        byte b = 127;
+        int d = 12 ;
+        d = b; // that is worked because int has more bytes than byte
+        System.out.println(d);
+
+        // Type Casting
+        float f = 5.6f;
+        int x = (int)f;
+        System.out.println(x);
+
+        byte g = 127;
+        int h = g;
+        System.out.println(h);
+
+        // Type Promotion  ( byte + byte -> can update to int)
+        byte first = 10;
+        byte second = 30;
+        int result = first * second;
+        System.out.println(result);
+    }
+}
